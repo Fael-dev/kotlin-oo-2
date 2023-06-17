157328643e04a4994f03663918cd55b301347953
Fael-dev <faelleonan@gmail.com>
2023-06-17 13:51:05 -0300

    Class Funcionario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     println("Bem-vindo ao Bytebank")
+
+    val funcionario = Funcionario(
+        "Rafael",
+        "000.000.000-00",
+        1200.00
+    )
+
+    println("Nome: ${funcionario.nome}")
+    println("CPF: ${funcionario.cpf}")
+    println("Salário: ${funcionario.salario}")
+    println("Bonificação: ${funcionario.bonificacao()}")
 }
