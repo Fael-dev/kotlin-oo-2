2888fcdefe45d3bfffe427e5834e10b6d8efe843
Fael-dev <faelleonan@gmail.com>
2023-06-17 14:07:00 -0300

    Class Gerente

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,4 +11,22 @@ fun main() {
     println("CPF: ${funcionario.cpf}")
     println("Salário: ${funcionario.salario}")
     println("Bonificação: ${funcionario.bonificacao()}")
+
+    val gerente = Gerente(
+        nome = "Rafael",
+        cpf = "000.000.000-00",
+        salario = 1200.00,
+        senha = 12345678
+    )
+
+    println("Nome: ${gerente.nome}")
+    println("CPF: ${gerente.cpf}")
+    println("Salário: ${gerente.salario}")
+    println("Bonificação: ${gerente.bonificacao()}")
+
+    if (gerente.autenticacao(12345678)) {
+        println("Autenticou com sucesso!")
+    } else {
+        println("Falha na autenticação")
+    }
 }
